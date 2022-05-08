20d2290b8380f74502ff5c13655ba20b7352fee6
VeronicaCorMe <vcordobam@unal.edu.co>
2022-05-07 21:38:20 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -9,35 +9,39 @@ import numpy as np
 import pandas as pd
 
 
+
+
 def pregunta_01():
     """
     En este punto se realiza la lectura de conjuntos de datos.
     Complete el código presentado a continuación.
     """
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
-
+    df = pd.read_csv("gm_2008_region.csv", sep=",", header=0, )
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = ____[____].____
-    X = ____[____].____
+    y = df["life"]
+    X = df["fertility"]
 
     # Imprima las dimensiones de `y`
-    print(____.____)
+    print(y.shape)
 
     # Imprima las dimensiones de `X`
-    print(____.____)
+    print(X.shape)
 
     # Transforme `y` a un array de numpy usando reshape
-    y_reshaped = y.reshape(____, ____)
+    y_reshaped = np.reshape(y, newshape=-1)
 
     # Trasforme `X` a un array de numpy usando reshape
-    X_reshaped = X.reshape(____, ____)
+    X_reshaped = np.reshape(X, newshape=-1)
 
     # Imprima las nuevas dimensiones de `y`
-    print(____.____)
+    print(y_reshaped.shape)
 
     # Imprima las nuevas dimensiones de `X`
-    print(____.____)
+    print(X_reshaped.shape)
+    
+
+
 
 
 def pregunta_02():
